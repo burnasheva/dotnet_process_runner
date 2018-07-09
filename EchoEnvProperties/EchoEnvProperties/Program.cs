aed4679d38bfdd1cd6d5b8af1a134a2db25c517b
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-07-09 15:45:21 +0200

    add project with env properties to the output

---
+++ EchoEnvProperties/EchoEnvProperties/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace EchoEnvProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("All Environment Variables:");
+            
+            foreach(DictionaryEntry e in System.Environment.GetEnvironmentVariables())
+            {
+                Console.WriteLine(e.Key  + ":" + e.Value);
+                
+            }
+        }
+    }
+}
